5e24d25b8b5501b687cf97678a6be6ea676186e1
J W Debelius <j.debelius@gmail.com>
2017-06-02 17:16:47 -0700

    adds functionality to continous

--- break4w/continous.py
+++ break4w/continous.py
@@ -5,9 +5,10 @@ from break4w.question import Question
 
 class Continous(Question):
 
-    def __init__(self, name, description, dtype=None, unit=None, limits=None,
-                 rounding=None, clean_name=None, mimarks=False, ontology=None,
-                 missing=None, blanks=None, colormap=None):
+    def __init__(self, name, description, unit, dtype=float, limits=None,
+        outliers=None, sig_figs=None, clean_name=None,
+        mimarks=False, ontology=None, missing=None, blanks=None,
+        colormap=None):
         """A Question object with continous responses
 
         Parameters
@@ -16,13 +17,33 @@ class Continous(Question):
             The name of column where the group is stored
         description : str
             A brief description of the data contained in the question
-        unit : str, optional
-            The unit of measure for the data type, if relevant. Units
-            are awesome, and should be included whenever relevant.
-        limits : two-elemant iterable
-            The range of values pertinant to analysis.
-        rounding : int, optional
-            The number of digits to which results should be rounded.
+        unit : str
+            The unit of measure for the data type. Unitless quantities can be
+            denoted with an empty string (`""`; i.e. reporting absorbance
+            values from a spectrophotometer). This is required, even if the
+            units are already recorded in the name. Units are awesome; without
+            them your data becomes absurd and meaningless. Public serivce
+            announcement over, back to your reguarly scheduled doc-string.
+        dtype : {int, float}
+            The datatype in which the responses should be represented.
+        limits: two element iterable of numbers, optional
+            Physical sonstraints on the value being measured. Essentially,
+            these describe values which it is not possible for the measurement
+            to take, due to constraints in the natural world.
+            For instance,  absloute concentrations cannot be negative. So, a
+            solution cannot contain -3 mM NaCl.
+            Limits can be expressed in both directions, or in a single
+            direction, with `None` replacing the missing value. So, for
+            concentration, the limit could be represented as `[0, None]`.
+        outliers : two element iterable of numbers, optional
+            The range of values pertinant to analysis. This is seperate form
+            the range of physical values *possible* for the data (provided in)
+            limits. In the same way as `limits`, bounds can be skipped using
+            `None` for the position.
+        sig_figs : int, optional
+            The number of signfiicant figures appropriate for the measurement.
+            This should be expressed as a decimal (i.e. `sig_figs = 0.1`),
+            specifying the unit of error on the value.
         clean_name : str, optional
             A nicer version of the way the column should be named. This can be
             used for display in figures. If nothing is provided, the column
@@ -50,10 +71,10 @@ class Continous(Question):
         ValueError
             When the values in `limits` are not expressed in order (i.e.
             the first value is larger than the second)
+        ValueError
+            When the values in `outliers` are not expressed in order (i.e.
+            the first value is larger than the second)
         """
-
-        if dtype is None:
-            dtype = float
 
         Question.__init__(self,
                           name=name,
@@ -66,19 +87,113 @@ class Continous(Question):
                           blanks=blanks,
                           colormap=colormap,
                           )
-        self.unit = unit
-        if limits is not None:
-            lower, upper = limits
-            if lower > upper:
-                raise ValueError('The lower limit cannot be greater than '
-                                 'the upper!')
-        else:
-            lower, upper = (None, None)
 
-        self.lower = lower
-        self.upper = upper
+        o_lower, o_upper = _check_limits(outliers, 'outliers')
+        a_lower, a_upper = _check_limits(limits, 'limits')
+
+        self.unit = unit
+        self.bound_lower = a_lower
+        self.bound_upper = a_upper
+        self.outlier_lower = o_lower
+        self.outlier_upper = o_upper
         self.type = 'Continous'
-        self.rounding = rounding
+        self.sig_figs = sig_figs
+
+    def validate_range(self, map_):
+        """
+        Checks values fall into the acceptable range for this type of data.
+
+        Parameters
+        ----------
+        map_ : DataFrame
+            A pandas object containing the data to be analyzed. The
+            Question `name` should be a column in the `map_`.
+
+        Raises
+        ------
+        ValueError
+            If the data in `map_` falls outside the specified limits
+        """
+        if (self.bound_upper is not None) and (self.bound_lower is not None):
+            update_text = ('The values were between %s and %s %s.'
+                           % (self.bound_lower, self.bound_upper, self.unit))
+        elif self.bound_upper is not None:
+            update_text = ('The values were less than or equal to %s %s.'
+                           % (self.bound_upper, self.unit))
+        elif self.bound_lower is not None:
+            update_text = ('The values were greater than or equal to %s %s.'
+                           % (self.bound_lower, self.unit))
+        else:
+            update_text = 'There were no limits specified.'
+
+        self.analysis_remap_dtype(map_)
+        lower_text = ''
+        lower_issue = False
+        if (self.bound_lower is not None):
+            if np.any(map_[self.name] < self.bound_lower):
+                lower_text = ('less than %s' % self.bound_lower)
+                lower_issue = True
+
+        upper_text = ''
+        upper_issue = False
+        if (self.bound_upper is not None):
+            if np.any(map_[self.name] > self.bound_upper):
+                upper_text = ('greater than %s'
+                              % (self.bound_upper))
+                upper_issue = True
+
+        error = False
+        if lower_issue and upper_issue:
+            error = True
+            error_string = ('There are values %s and %s %s.'
+                            % (lower_text, upper_text, self.unit))
+        elif lower_issue:
+            error = True
+            error_string = ('There are values %s %s.'
+                            % (lower_text, self.unit))
+        elif upper_issue:
+            error = True
+            error_string = ('There are values %s %s.'
+                            % (upper_text, self.unit))
+
+        if error:
+            self._update_log('Validate the mapping file', 'error',
+                             error_string)
+            raise ValueError(error_string)
+        else:
+            self._update_log('Validate the mapping file', 'pass',
+                             update_text)
+
+    def dictionary_update_outliers(self, outliers):
+        """
+        Updates the bounds for outliers in the metadata column.
+
+        Parameters
+        ----------
+        outliers : two element iterable of numbers, optional
+            The range of values pertinant to analysis. This is seperate form
+            the range of physical values *possible* for the data (provided in)
+            limits.
+            Outlires can be expressed in both directions, or in a single
+            direction, with `None` replacing the missing value. So, for
+            concentration, the limit could be represented as `[0, None]`.
+
+        Raises
+        ------
+        ValueError
+            When the values in `outliers` are not expressed in order (i.e.
+            the first value is larger than the second)
+
+        """
+        ori_lower = self.outlier_lower
+        ori_upper = self.outlier_upper
+        lower, upper = _check_limits(outliers, 'outliers')
+        self.outlier_lower = lower
+        self.outlier_upper = upper
+        self._update_log('update outlier values', 'update dictionary',
+                         'The outlier values have been updated as %s > %s'
+                         'and %s > %s.' % (ori_lower, lower, ori_upper, upper)
+                         )
 
     def analysis_drop_outliers(self, map_):
         """
@@ -87,28 +202,96 @@ class Continous(Question):
         Parameters
         ----------
         map_ : DataFrame
-            A pandas dataframe containing the column described by the question
-            name.
+            A pandas object containing the data to be analyzed. The
+            Question `name` should be a column in the `map_`.
+
+        """
+        self.analysis_remap_dtype(map_)
+
+        if ((self.outlier_lower is not None) and
+                (self.outlier_upper is not None)):
+            def remap_(x):
+                if x < self.outlier_lower:
+                    return np.nan
+                elif x > self.outlier_upper:
+                    return np.nan
+                else:
+                    return x
+            summary = ('values outside [%s, %s]'
+                       % (self.outlier_lower, self.outlier_upper))
+        elif (self.outlier_lower is not None):
+            def remap_(x):
+                if x < self.outlier_lower:
+                    return np.nan
+                else:
+                    return x
+            summary = ('values less than %s' % self.outlier_lower)
+        elif (self.outlier_upper is not None):
+            def remap_(x):
+                if x > self.outlier_upper:
+                    return np.nan
+                else:
+                    return x
+            summary = ('values greater than %s' % self.outlier_upper)
+        else:
+            def remap_(x):
+                return x
+            summary = 'No values dropped'
+
+        map_[self.name] = map_[self.name].apply(remap_)
+        self._update_log('drop outliers', 'drop',
+                         summary)
+
+    def analysis_set_sig_figs(self, map_):
+        """
+        Rounds the continous values to the correct significant figures.
+
+        Significant figures are awesome (along with units). This function
+        is designed to help you maintain the correct significant digits
+        for the value measured.
+
+        Parameters
+        ----------
+        map_ : DataFrame
+            A pandas object containing the data to be analyzed. The
+            Question `name` should be a column in the `map_`.
 
         Raises
         ------
         ValueError
-            If the column identified by the question object is not part of the
-            supplied DataFrame.
+            If the sig_figs parameter has not been specified.
+
         """
-        self.check_map(map_)
         self.analysis_remap_dtype(map_)
 
-        if self.lower is not None:
-            def remap_(x):
-                if x < self.lower:
-                    return np.nan
-                elif x > self.upper:
-                    return np.nan
-                else:
-                    return x
+        if self.sig_figs is None:
+            raise ValueError('Rounding must be defined!')
 
-            map_[self.name] = map_[self.name].apply(remap_)
-            self._update_log('drop outliers', 'drop',
-                             'values outside [%f, %f]'
-                             % (self.lower, self.upper))
+        # if self.dtype is float:
+        def remap_(x):
+            return np.round(x, -int(np.floor(np.log10(self.sig_figs))))
+
+        map_[self.name] = map_[self.name].apply(remap_)
+        for l in [self.bound_lower, self.bound_upper,
+                  self.outlier_lower, self.outlier_upper]:
+            if l is not None:
+                l = remap_(l)
+
+        self._update_log('round significant figures', 'correct',
+                         'Rounded the data to the nearest %s.'
+                         % self.sig_figs)
+
+def _check_limits(limits, var_name):
+    """
+    A helper function that checks whether the limit value meets the criteria
+    """
+    if limits is not None:
+            lower, upper = limits
+            if ((lower is not None) and (upper is not None) and
+                    (lower > upper)):
+                raise ValueError('The lower limit cannot be greater than '
+                                 'the upper for %s.')
+    else:
+        lower, upper = (None, None)
+
+    return lower, upper
--- break4w/tests/test_continous.py
+++ break4w/tests/test_continous.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import pandas.util.testing as pdt
 
-from break4w.continous import Continous
+from break4w.continous import Continous, _check_limits
 
 
 class ContinousTest(TestCase):
@@ -23,45 +23,103 @@ class ContinousTest(TestCase):
                             "Bitty's frog year")
         self.dtype = int
         self.unit = 'years'
-        self.range = [0, 4]
 
         self.c = Continous(self.name,
                            self.description,
                            unit=self.unit,
                            dtype=self.dtype,
-                           limits=self.range)
+                           limits=[1, None],
+                           outliers=[None, 5])
 
     def test_init_default(self):
         test = Continous(
             name=self.name,
             description=self.description,
+            unit=self.unit
             )
+        self.assertEqual(test.unit, self.unit)
         self.assertEqual('Continous', test.type)
         self.assertEqual(test.dtype, float)
-        self.assertEqual(test.lower, None)
-        self.assertEqual(test.upper, None)
+        self.assertEqual(test.bound_lower, None)
+        self.assertEqual(test.bound_upper, None)
+        self.assertEqual(test.outlier_lower, None)
+        self.assertEqual(test.outlier_upper, None)
+        self.assertEqual(test.sig_figs, None)
 
     def test_init(self):
-        self.assertEqual(self.c.unit, self.unit)
-        self.assertEqual(self.c.lower, 0)
-        self.assertEqual(self.c.upper, 4)
-        self.assertEqual(self.c.dtype, int)
+        self.assertEqual(self.c.dtype, self.dtype)
+        self.assertEqual(self.c.bound_lower, 1)
+        self.assertEqual(self.c.bound_upper, None)
+        self.assertEqual(self.c.outlier_lower, None)
+        self.assertEqual(self.c.outlier_upper, 5)
 
-    def test_init_error(self):
-        with self.assertRaises(ValueError):
-            Continous('TEST_COLUMN',
-                      'Testing. 1, 2, 3.\nAnything but that!',
-                      limits=[12, 5])
+    def test_update_outliers(self):
+        self.assertEqual(self.c.outlier_lower, None)
+        self.assertEqual(self.c.outlier_upper, 5)
+        self.c.dictionary_update_outliers([2, 7])
+        self.assertEqual(self.c.outlier_lower, 2)
+        self.assertEqual(self.c.outlier_upper, 7)
 
-    def test_analysis_drop_outliers(self):
+    def test_analysis_drop_outliers_none(self):
         pdt.assert_series_equal(
             self.map_['years_on_team'],
             pd.Series(['1', '2', '2', '4'],
                       index=['Bitty', 'Ransom', 'Holster', 'Johnson'],
                       name='years_on_team')
             )
-        self.c.lower = 2
-        self.c.upper = 3
+        self.c.dictionary_update_outliers([None, None])
+        self.c.analysis_drop_outliers(self.map_)
+        pdt.assert_series_equal(
+            self.map_['years_on_team'],
+            pd.Series([1, 2, 2, 4],
+                      index=['Bitty', 'Ransom', 'Holster', 'Johnson'],
+                      name='years_on_team')
+            )
+        self.assertEqual(self.c.log[2]['transformation'], 'No values dropped')
+
+    def test_analysis_drop_outliers_left(self):
+        pdt.assert_series_equal(
+            self.map_['years_on_team'],
+            pd.Series(['1', '2', '2', '4'],
+                      index=['Bitty', 'Ransom', 'Holster', 'Johnson'],
+                      name='years_on_team')
+            )
+        self.c.dictionary_update_outliers([2, None])
+        self.c.analysis_drop_outliers(self.map_)
+        pdt.assert_series_equal(
+            self.map_['years_on_team'],
+            pd.Series([np.nan, 2, 2, 4],
+                      index=['Bitty', 'Ransom', 'Holster', 'Johnson'],
+                      name='years_on_team')
+            )
+        self.assertEqual(self.c.log[2]['transformation'], 'values less than 2')
+
+    def test_analysis_drop_outliers_right(self):
+        pdt.assert_series_equal(
+            self.map_['years_on_team'],
+            pd.Series(['1', '2', '2', '4'],
+                      index=['Bitty', 'Ransom', 'Holster', 'Johnson'],
+                      name='years_on_team')
+            )
+        self.c.dictionary_update_outliers([None, 3])
+        self.c.analysis_drop_outliers(self.map_)
+        pdt.assert_series_equal(
+            self.map_['years_on_team'],
+            pd.Series([1, 2, 2, np.nan],
+                      index=['Bitty', 'Ransom', 'Holster', 'Johnson'],
+                      name='years_on_team')
+            )
+        self.assertEqual(self.c.log[2]['transformation'],
+                         'values greater than 3')
+
+    def test_analysis_drop_outliers_both(self):
+        pdt.assert_series_equal(
+            self.map_['years_on_team'],
+            pd.Series(['1', '2', '2', '4'],
+                      index=['Bitty', 'Ransom', 'Holster', 'Johnson'],
+                      name='years_on_team')
+            )
+        self.c.dictionary_update_outliers([2, 3])
         self.c.analysis_drop_outliers(self.map_)
         pdt.assert_series_equal(
             self.map_['years_on_team'],
@@ -69,7 +127,109 @@ class ContinousTest(TestCase):
                       index=['Bitty', 'Ransom', 'Holster', 'Johnson'],
                       name='years_on_team')
             )
+        self.assertEqual(self.c.log[2]['transformation'],
+                         'values outside [2, 3]')
 
+    def test_analysis_set_sig_figs_error(self):
+        with self.assertRaises(ValueError):
+            self.c.analysis_set_sig_figs(self.map_)
+
+    def test_analysis_set_sig_figs(self):
+        pdt.assert_series_equal(
+            self.map_['years_on_team'],
+            pd.Series(['1', '2', '2', '4'],
+                      index=['Bitty', 'Ransom', 'Holster', 'Johnson'],
+                      name='years_on_team')
+            )
+        self.c.sig_figs = 0.1
+        self.c.analysis_set_sig_figs(self.map_)
+        pdt.assert_series_equal(
+            self.map_['years_on_team'],
+            pd.Series([1, 2, 2, 4],
+                      index=['Bitty', 'Ransom', 'Holster', 'Johnson'],
+                      name='years_on_team')
+            )
+        self.assertEqual(self.c.bound_lower, 1.0)
+        self.assertEqual(self.c.bound_upper, None)
+        self.assertEqual(self.c.outlier_lower, None)
+        self.assertEqual(self.c.outlier_upper, 5.0)
+
+    def test_validate_range_none_pass(self):
+        self.c.bound_upper = None
+        self.c.bound_lower = None
+        self.c.validate_range(self.map_)
+        self.assertEqual(self.c.log[1]['transformation'],
+                         'There were no limits specified.')
+
+    def test_validate_range_lower_pass(self):
+        self.c.validate_range(self.map_)
+        self.assertEqual(self.c.log[1]['transformation'],
+                         'The values were greater than or equal to 1 years.')
+
+    def test_validate_range_lower_error(self):
+        self.c.bound_lower = 2
+        with self.assertRaises(ValueError):
+            self.c.validate_range(self.map_)
+        self.assertEqual(self.c.log[1]['transformation'],
+                         'There are values less than 2 years.')
+
+    def test_validate_range_upper_pass(self):
+        self.c.bound_upper = 5
+        self.c.bound_lower = None
+        self.c.validate_range(self.map_)
+        self.assertEqual(self.c.log[1]['transformation'],
+                         'The values were less than or equal to 5 years.')
+
+    def test_validate_range_upper_error(self):
+        self.c.bound_upper = 1
+        with self.assertRaises(ValueError):
+            self.c.validate_range(self.map_)
+        self.assertEqual(self.c.log[1]['transformation'],
+                         'There are values greater than 1 years.')
+
+    def test_validate_range_both_pass(self):
+        self.c.bound_upper = 5
+        self.c.validate_range(self.map_)
+        self.assertEqual(self.c.log[1]['transformation'],
+                         'The values were between 1 and 5 years.')
+
+    def test_validate_range_both_error(self):
+        self.c.bound_lower = 2
+        self.c.bound_upper = 3
+        with self.assertRaises(ValueError):
+            self.c.validate_range(self.map_)
+        self.assertEqual(self.c.log[1]['transformation'],
+                         'There are values less than 2 and greater than'
+                         ' 3 years.')
+
+    def test_check_limits_none(self):
+        limits = None
+        lower, upper = _check_limits(limits, 'limits')
+        self.assertEqual(lower, None)
+        self.assertEqual(upper, None)
+
+    def test_check_limits_left_none(self):
+        limits = [None, 5]
+        lower, upper = _check_limits(limits, 'limits')
+        self.assertEqual(lower, None)
+        self.assertEqual(upper, 5)
+
+    def test_check_limits_right_none(self):
+        limits = [-3, None]
+        lower, upper = _check_limits(limits, 'limits')
+        self.assertEqual(lower, -3)
+        self.assertEqual(upper, None)
+
+    def test_check_limits_all_pass(self):
+        limits = [-3, 5]
+        lower, upper = _check_limits(limits, 'limits')
+        self.assertEqual(lower, -3)
+        self.assertEqual(upper, 5)
+
+    def test_check_limits_all_error(self):
+        limits = [8, 5]
+        with self.assertRaises(ValueError):
+            _check_limits(limits, 'limits')
 
 if __name__ == '__main__':
     main()
